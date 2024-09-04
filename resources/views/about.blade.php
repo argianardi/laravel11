41dbd6043bfed2ea6be3512e04e7ff0b26cc8a77
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-09-04 10:39:14 +0700

    Feat: Add heading section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.layout')
 
 @section('title', 'About Us')
-
+@section('heading', 'About')
 @section('content')
-    <h1>About Us</h1>
+    <h1 class="text-3xl font-bold tracking-tight text-red-900 underline">About Us</h1>
     <p>Ini adalah halaman about.</p>
 @endsection
