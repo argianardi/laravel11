5290f99d61b3ec41d59d77932080ff1a0262e554
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-09-04 10:41:19 +0700

    Feat: Add new layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,8 @@
-<x-app-layout title="Contact">
-    Contact
-</x-app-layout>
+@extends('layouts.layout')
+
+@section('title', 'Contact')
+{{-- @section('heading', 'Contact') --}}
+
+@section('content')
+    <p class="text-xl font-bold tracking-tight text-blue-900">Contact</p>
+@endsection
