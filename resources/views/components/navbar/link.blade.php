56cd3822c5570d80957314733071121f37b27737
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-09-04 11:37:37 +0700

    Feat: Add active state

--- resources/views/components/navbar/link.blade.php
+++ resources/views/components/navbar/link.blade.php
@@ -1,4 +1,3 @@
-<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-{{-- aria-current="page"  --}}
+{{-- resources/views/components/navbar/link.blade.php --}}
 <a {{ $attributes }}
-    class="hover:bg-gray-700 hover:text-white px-3 py-2 text-sm font-medium text-gray-300 rounded-md">{{ $slot }}</a>
+    class="{{ request()->fullUrlIs(url($href)) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}  px-3 py-2 text-sm font-medium rounded-md">{{ $slot }}</a>
